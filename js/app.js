5f212a331fc2e434a60711776bfb3aadeb0e2b9c
Olabenjo Tunde <t.visualb@hotmail.com>
2015-03-17 13:53:23 +0200

    reconstructed json file and added appListController

--- js/app.js
+++ js/app.js
@@ -12,3 +12,13 @@
   #######################################################################*/
 var app = angular.module('naijaApp', []); //The app module
 
+//creating a controller object incase we need more controllers
+var controllers = {};
+
+//we get the list from the applist.json
+controllers.AppListController = function($scope, $http) {
+    $http.get('./applist.json').success(function(data) {
+    $scope.applist = data.apps;
+  });
+
+app.controller(controllers);
