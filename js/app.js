01da208939890e49e497ab509987f2db18700a09
Olabenjo Tunde <t.visualb@hotmail.com>
2015-03-17 15:18:20 +0200

    cleaned up index

--- js/app.js
+++ js/app.js
@@ -4,10 +4,12 @@
   http://twitter.com/tbenjis
   The script reads all app data from applist.json and displays
   them with filters.
+  image folder
+  /img/apps - must contain a 228x228 logo of the app < 40kb
 
-  Controler
-  ---------
-  AppsController
+  Controlers
+  ----------
+  AppListController
 
   #######################################################################*/
 var app = angular.module('naijaApp', []); //The app module
@@ -15,7 +17,7 @@ var app = angular.module('naijaApp', []); //The app module
 //creating a controller object incase we need more controllers
 var controllers = {};
 
-//we get the list from the applist.json
+//we get the list from applist.json
 controllers.AppListController = function($scope, $http) {
     $http.get('./applist.json').success(function(data) {
     $scope.applist = data.apps;
