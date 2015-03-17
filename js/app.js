3f90ac1572dc30ca1f78ed0d34b8c10ecb79060e
Olabenjo Tunde <t.visualb@hotmail.com>
2015-03-17 13:11:26 +0200

    initialized app.js

---
+++ js/app.js
@@ -0,0 +1,14 @@
+/*#######################################################################
+  
+  naijaApp
+  http://twitter.com/tbenjis
+  The script reads all app data from applist.json and displays
+  them with filters.
+
+  Controler
+  ---------
+  AppsController
+
+  #######################################################################*/
+var app = angular.module('naijaApp', []); //The app module
+
