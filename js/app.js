6d862d83ad8f56d0bdda1d1488742a8563ab90ee
Olabenjo Tunde <t.visualb@hotmail.com>
2015-03-25 18:55:43 +0200

    added chart

--- js/app.js
+++ js/app.js
@@ -20,13 +20,42 @@ var controllers = {};
 //lets sort the app list at random
 function randSrt(){
       return (Math.round(Math.random())-0.5); 
-    }
+}
 
 //we get the list from applist.json
-controllers.AppListController = function($scope, $http) {
-    $http.get('./applist.json').success(function(data) {
-    $scope.applist = data.apps.sort(randSrt); //sort the applist here
+controllers.AppListController = function($scope, $http, $filter) {
+    
+  $http.get('./applist.json').success(function(data) {
+  $scope.applist = data.apps.sort(randSrt); //sort the applist here    
+
+  //we count both android and IOS apps for the graph 
+  getCount = function(strType){
+    return $filter('filter')( $scope.applist, {$:strType}, true).length
+  }
+
+  //do a little chart data
+  var doughnutData = [
+          {
+            value: getCount('Android'),
+            color:"#27ae60",
+            highlight: "#2ecc71",
+            label: "Android"
+          },
+          {
+            value: getCount('IOS'),
+            color: "#c0392b",
+            highlight: "#e74c3c",
+            label: "IOS"
+          }
+        ];
+
+  //draw the chat
+  var ctx = document.getElementById("chart-area").getContext("2d");
+  var myDoughnut = new Chart(ctx).Doughnut(doughnutData, {responsive : true});
+
   });
+
 }
 
 app.controller(controllers);
+
