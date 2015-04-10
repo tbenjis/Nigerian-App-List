89523712c00bf751414698936e45bdf9ab021b0e
unknown <babasegun.adeleye@gmail.com>
2015-04-10 18:37:13 +0100

    Nigerian-App-List\css\style.css made changes to css Nigerian-App-List\index.html added pagination links Nigerian-App-List\js\app.js added pagination script

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@
   AppListController
 
   #######################################################################*/
-var app = angular.module('naijaApp', []); //The app module
+var app = angular.module('naijaApp', ['ui.bootstrap']); //The app module
 
 //creating a controller object incase we need more controllers
 var controllers = {};
@@ -20,13 +20,58 @@ var controllers = {};
 //lets sort the app list at random
 function randSrt(){
       return (Math.round(Math.random())-0.5); 
-    }
+}
 
+app.filter('startFrom', function () {
+  return function (input, start) {
+    if (input) {
+      start = +start;
+      return input.slice(start);
+    }
+    return [];
+  };
+});
 //we get the list from applist.json
-controllers.AppListController = function($scope, $http) {
-    $http.get('./applist.json').success(function(data) {
-    $scope.applist = data.apps.sort(randSrt); //sort the applist here
+controllers.AppListController = function($scope, $http, $filter) {
+
+  $http.get('./applist.json').success(function(data) {
+  $scope.applist = data.apps.sort(randSrt); //sort the applist here    
+
+  //pagination settings
+   $scope.currentPage = 1;
+   $scope.perPage =6;
+   $scope.total = $scope.applist.length;
+   $scope.totalPages = Math.ceil($scope.total/$scope.perPage);
+
+  
+  //we count both android and IOS apps for the graph 
+  getCount = function(strType){
+    return $filter('filter')( $scope.applist, {$:strType}, true).length
+  }
+
+  //do a little chart data
+  var doughnutData = [
+          {
+            value: getCount('Android'),
+            color:"#27ae60",
+            highlight: "#2ecc71",
+            label: "Android"
+          },
+          {
+            value: getCount('IOS'),
+            color: "#c0392b",
+            highlight: "#e74c3c",
+            label: "IOS"
+          }
+        ];
+
+  //draw the chat
+  var ctx = document.getElementById("chart-area").getContext("2d");
+  var myDoughnut = new Chart(ctx).Doughnut(doughnutData, {responsive : true});
+
   });
+
 }
 
 app.controller(controllers);
+
